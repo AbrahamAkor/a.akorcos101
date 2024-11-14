4bb901894a3a24bb087162edc7bcb42cf55a1c30
AbrahamAkor <abraham.akor@pau.edu.ng>
2024-11-14 20:39:43 +0100

    week-3

---
+++ week-4/practice-1.rs
@@ -0,0 +1,30 @@
+use std::io;
+
+fn main() {
+    println!("Student Information Management System");
+
+    println!("\nPlease enter your name.");
+    let mut name = String::new();
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read input");
+    let name = name.trim();
+    println!("Your name is: {}", name);
+
+
+    println!("\nEnter your age.");
+    let mut age = String::new();
+    io::stdin()
+        .read_line(&mut age)
+        .expect("Failed to read input");
+    
+    let age: i32 = match age.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid input, age must be an integer.");
+            return;
+        }
+    };
+    
+    println!("Your age is: {}", age);
+}
